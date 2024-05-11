90f1be12dc77259119760ca682f697e3852b9666
ArnoldRamodike <riccoarnold23@gmail.com>
2024-05-11 09:05:20 +0200

    Convex APIs creation and Schama  and reading/creating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,21 @@
 import { Button } from "@/components/ui/button";
 import { SignedOut } from "@clerk/clerk-react";
 import { SignedIn, SignInButton, SignOutButton, useSession } from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
   const session = useSession();
+  const createfile = useMutation(api.files.createFile)
+  const files = useQuery(api.files.getFiles);
+
+  console.log(files);
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <SignedIn>
         <SignOutButton>
-        <Button>Sign Out</Button>
+          <Button>Sign Out</Button>
         </SignOutButton>
       </SignedIn>
       <SignedOut>
@@ -18,8 +25,10 @@ export default function Home() {
          <Button>Sign in</Button>
          </SignInButton>
       </SignedOut>
-     
-      <Button>This is the start</Button>
+     {files?.map((item) => {
+      return <div key={item.id}>{item.name}</div>
+     })}
+      <Button onClick={() => {createfile({name: 'Hello World'})}}>This is the start</Button>
     </main>
   );
 }
